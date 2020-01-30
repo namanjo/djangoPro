65f7e6f1f222d9358c36b3dfb6f309035d35b52d
Naman Joshi <naman14joshi@gmail.com>
2020-01-30 23:42:48 +0530

    Removed hard coded urls.
    Removed all the hard coaded urls and created base.html file.

--- app_one/urls.py
+++ app_one/urls.py
@@ -2,6 +2,10 @@ from app_one import views
 from django.urls import path
 
 
+#For template tagging
+app_name = 'app_one'
+
+
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('signup/', views.sign_up, name = 'sign_up'),
